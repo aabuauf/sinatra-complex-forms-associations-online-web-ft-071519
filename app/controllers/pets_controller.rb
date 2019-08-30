0d669713532152b7ba0cb330ead8929d4533ef2a
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-30 22:27:13 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -45,7 +45,7 @@ patch '/pets/:id' do
   
     if !params["owner"]["name"].empty?
       binding.pry
-      @pet.pets = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
       @pet.save
     end
     redirect "pets/#{@pet.id}"
